24694d60d742a1b624f9f2e91cb93f8bcbd0cae5
ElavarasiMarkandan <emarkandan@ancestry.com>
2024-07-11 17:47:18 -0600

    remove unwanted files

--- docker/app/startup.sh
+++
@@ -1,6 +0,0 @@
-exec java \
-  -javaagent:/app/apm-agent.jar \
-  -Delastic.apm.service_name=roman-numeral-converter \
-  -Delastic.apm.server_url=http://apm-server:8200 \
-  -Delastic.apm.application_packages=com.romannumeral \
-  -jar app.jar
